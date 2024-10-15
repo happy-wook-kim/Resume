8787a849027a657fc7db269650504575b4bf9001
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2024-10-15 09:40:58 +0900

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://happy-wook-kim.github.io/Resume";
+
+const nextConfig = {
+    output: 'export',
+    basePath: '/Resume',
+    images: {
+        unoptimized: true,
+    },
+    reactStrictMode: true,
+    // assetPrefix: !debug ? repository : "", // production 일때 prefix 경로
+    // trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+};
 
 export default nextConfig;
